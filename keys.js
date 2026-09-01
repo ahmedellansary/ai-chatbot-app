7cfe9122b7249283a368bf2e1bc307eda902196d
ahmedellansary <Ahmed.ellansary@gmail.com>
2026-09-01 10:39:35 +0300

    🚀 Deploy: keys.js

--- keys.js
+++ keys.js
@@ -1,16 +1,19 @@
-// Keys Setup Script
-(function() {
-  const _k1 = ['sk-or-v1-', 'b82e11595ed064e7', '51bfff2b251a4c54', 'ca0da9bc779786cd', 'baf933e916398e03'].join('');
-  const _k2 = [
-    ['gsk_6ULPilUmj8gf0mbu2ZlX', 'WGdyb3FYkqImKQ7lZPdjGIBERqBKrDhX'].join(''),
-    ['gsk_ECkO3AaJ8sBRAnd7gLMN', 'WGdyb3FYTMBNYK0SxQU6W1CSXEx23koB'].join(''),
-    ['gsk_QiThrmueUOxgPM9xcIwn', 'WGdyb3FYVF37eSLhIgG9RYTXakzxc16l'].join(''),
-    ['gsk_dVkSeAKGE0wQRxqy7OWX', 'WGdyb3FY0vHBuaJxlmnjbaPytbsl4dn8'].join(''),
-    ['gsk_u5bCiNIx7oQaS2XzqiAG', 'WGdyb3FYE6s7QoY0qntIUhBU4D13AhjZ'].join('')
-  ].join(',');
-  const _k3 = ['ghp_Ep2hC2i0', 'LFNVeyCSiUFlMMb0', '5ILzmJ2nzGGN'].join('');
+// Runtime configuration bootstrap: keep secrets out of source control.
+(function () {
+  const safeConfig = {
+    OPENROUTER_API_KEY: '',
+    GROQ_API_KEY: '',
+    GITHUB_TOKEN: ''
+  };
 
-  localStorage.setItem('OPENROUTER_API_KEY', _k1);
-  localStorage.setItem('GROQ_API_KEY', _k2);
-  localStorage.setItem('GITHUB_TOKEN', _k3);
+  if (!window.__APP_CONFIG__) {
+    window.__APP_CONFIG__ = {};
+  }
+
+  Object.entries(safeConfig).forEach(([key, value]) => {
+    window.__APP_CONFIG__[key] = value;
+    try {
+      localStorage.setItem(key, value);
+    } catch {}
+  });
 })();
